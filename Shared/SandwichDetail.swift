2d6f460d4322137bb284f16b1422f7a20845027e
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2020-12-21 05:58:45 +0100

    Added toolbar, nagation link to sandwich detail view and display total number of sandwiches

---
+++ Shared/SandwichDetail.swift
@@ -0,0 +1,20 @@
+//
+//  SandwichDetail.swift
+//  Sandwiches
+//
+//  Created by Evidence Osikhena on 21/12/2020.
+//
+
+import SwiftUI
+
+struct SandwichDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SandwichDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        SandwichDetail()
+    }
+}
