d18a98baec353e0db3470fa21bef4b78a5e4090b
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2020-12-21 04:20:23 +0100

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Evidence Osikhena on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
