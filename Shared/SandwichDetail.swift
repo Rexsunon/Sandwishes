f998bc473fa0bff8551b6946015a05a30f0c2c0d
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2020-12-21 17:15:17 +0100

    Added zoom and animation capability and added a label to show spicy sandwiches

--- Shared/SandwichDetail.swift
+++ Shared/SandwichDetail.swift
@@ -12,18 +12,42 @@ struct SandwichDetail: View {
     @State private var zoomed = false
     
     var body: some View {
-        Image(sandwich.imageName)
-            .resizable()
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .onTapGesture { zoomed.toggle() }
-            .navigationTitle(sandwich.name)
+        VStack {
+            Spacer(minLength: 0)
+            
+            Image(sandwich.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        zoomed.toggle()
+                    }
+                }
+            
+            Spacer(minLength: 0)
+            
+            if sandwich.isSpicy && !zoomed {
+                HStack {
+                    Spacer()
+                    Label("Spicy", systemImage: "flame.fill")
+                    Spacer()
+                }
+                .padding(.all)
+                .font(Font.headline.smallCaps())
+                .background(Color.red)
+                .foregroundColor(.yellow)
+                .transition(.move(edge: .bottom))
+            }
+        }
+        .navigationTitle(sandwich.name)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 struct SandwichDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            SandwichDetail(sandwich: testData[0])
+            SandwichDetail(sandwich: testData[1])
         }
     }
 }
